2709fe51e388ea6fbbfab06839826caf76a1a952
SukhpreetO1 <ssingh77022@gmail.com>
2024-02-06 19:01:07 +0530

    add the dropdown file

---
+++ users/navbar/men_dropdown/men_dropdown_list.php
@@ -0,0 +1,125 @@
+<link rel="stylesheet" href="<?php echo $_ENV['BASE_URL'] ?>/users/navbar/men_dropdown/men_dropdown_list.css">
+<div class="men_list">
+    <div class="section_part_1">
+        <div class="section1">
+            <h6 class="men_section_heading">Topwear</h6>
+            <ul>
+                <li><a href="#">T-Shirts</a></li>
+                <li><a href="#">Casual Shirts</a></li>
+                <li><a href="#">Formal Shirts</a></li>
+                <li><a href="#">Sweatshirts</a></li>
+                <li><a href="#">Sweaters</a></li>
+                <li><a href="#">Jackets</a></li>
+                <li><a href="#">Blazers & Coats</a></li>
+                <li><a href="#">Suits</a></li>
+                <li><a href="#">Rain Jackets</a></li>
+            </ul>
+        </div>
+        <div class="section2">
+            <h6 class="men_section_heading">Indians & Festive Wear</h6>
+            <ul>
+                <li><a href="#">Kurtas & Kurta Sets</a></li>
+                <li><a href="#">Sherwanis</a></li>
+                <li><a href="#">Nehru Jackets</a></li>
+                <li><a href="#">Dhotis</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="section_part_2">
+        <div class="section3">
+            <h6 class="men_section_heading">Bottomwear</h6>
+            <ul>
+                <li><a href="#">Jeans</a></li>
+                <li><a href="#">Casual Trousers/a></li>
+                <li><a href="#">Formal Trousers</a></li>
+                <li><a href="#">Shorts</a></li>
+                <li><a href="#">Track Pants & Joggers</a></li>
+            </ul>
+        </div>    
+        <div class="section4">
+            <h6 class="men_section_heading">Innerwear & sleepwear</h6>
+            <ul>
+                <li><a href="#">Briefs & Trunks</a></li>
+                <li><a href="#">Boxers</a></li>
+                <li><a href="#">Vests</a></li>
+                <li><a href="#">Sleepwear & Loungewear</a></li>
+                <li><a href="#">Thermals</a></li>
+            </ul>
+        </div>
+        <div class="section5">
+            <h6 class="men_section_heading">Plus Size</h6>
+        </div>
+    </div>
+    <div class="section_part_3">
+        <div class="section6">
+            <h6 class="men_section_heading">Footwear</h6>
+            <ul>
+                <li><a href="#">Casual Shoes</a></li>
+                <li><a href="#">Sports Shoes</a></li>
+                <li><a href="#">Formal Shoes</a></li>
+                <li><a href="#">Sneakers</a></li>
+                <li><a href="#">Sandals & Floaters</a></li>
+                <li><a href="#">Flip Flops</a></li>
+                <li><a href="#">Socks</a></li>
+            </ul>
+        </div>    
+        <div class="section7">
+            <h6 class="men_section_heading">Personal Care & Grooming</h6>
+        </div>
+        <div class="section8">
+            <h6 class="men_section_heading">Sunglasses & Frames</h6>
+        </div>
+        <div class="section9">
+            <h6 class="men_section_heading">Watches</h6>
+        </div>
+    </div>
+    <div class="section_part_4">
+        <div class="section10">
+            <h6 class="men_section_heading">Sports & Active Wear</h6>
+            <ul>
+                <li><a href="#">Sports Shoes</a></li>
+                <li><a href="#">Sports Sandals</a></li>
+                <li><a href="#">Active T-Shirts</a></li>
+                <li><a href="#">Track Pants & Shorts</a></li>
+                <li><a href="#">Tracksuits</a></li>
+                <li><a href="#">Jackets & Sweatshirts</a></li>
+                <li><a href="#">Sports Accessories</a></li>
+                <li><a href="#">Swimwear</a></li>
+            </ul>
+        </div>    
+        <div class="section11">
+            <h6 class="men_section_heading">Gadgets</h6>
+            <ul>
+                <li><a href="#">Smart Wearables</a></li>
+                <li><a href="#">Fitness Gadgets</a></li>
+                <li><a href="#">Headphones</a></li>
+                <li><a href="#">Speakers</a></li>
+            </ul>
+        </div>
+    </div> 
+    <div class="section_part_5">
+        <div class="section12">
+            <h6 class="men_section_heading">Fashion Accessories</h6>
+            <ul>
+                <li><a href="#">Wallets</a></li>
+                <li><a href="#">Belts</a></li>
+                <li><a href="#">Perfumes & Body Mists</a></li>
+                <li><a href="#">Trimmers</a></li>
+                <li><a href="#">Deodorants</a></li>
+                <li><a href="#">Ties, Cufflinks & Pocket Squares</a></li>
+                <li><a href="#">Accessory Gift Sets</a></li>
+                <li><a href="#">Caps & Hats</a></li>
+                <li><a href="#">Mufflers, Scarves & Gloves</a></li>
+                <li><a href="#">Phone Cases</a></li>
+                <li><a href="#">Rings & Wristwear</a></li>
+                <li><a href="#">Helmets</a></li>
+            </ul>
+        </div>    
+        <div class="section13">
+            <h6 class="men_section_heading">Bags & Backpacks</h6>
+        </div>
+        <div class="section14">
+            <h6 class="men_section_heading">Luggages & Trolleys</h6>
+        </div>
+    </div>
+</div>
