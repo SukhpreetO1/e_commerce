5919c35091765471b93303de4fc7aeed0315fd2c
SukhpreetO1 <ssingh77022@gmail.com>
2024-02-27 11:47:00 +0530

    fixed the bug of redirection when page refresh

--- admin/homepage/index/index.js
+++ admin/homepage/index/index.js
@@ -26,56 +26,83 @@ function handle_ajax_redirection(url) {
     });
 }
 
+function handle_redirection_and_push_state(urlPath, urlName) {
+    handle_ajax_redirection('/admin' + urlPath + '.php');
+    var newUrl = BASE_URL + '/admin/homepage/index/index.php?' + urlName;
+    history.pushState(null, null, newUrl);
+}
+
 // redirection ajax when click on sidebar text / logo
 $(document).on('click', '.title', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/homepage/dashboard/dashboard.php');
+    handle_redirection_and_push_state('/homepage/dashboard/dashboard', 'dashboard');
 });
 
 // redirection ajax when click on dashboard
 $(document).on('click', '.dashboard', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/homepage/dashboard/dashboard.php');
-});
-
-// redirection ajax when click on products
-$(document).on('click', '.products', function (e) {
-    e.preventDefault();
-    handle_ajax_redirection('/admin/products/products.php');
+    handle_redirection_and_push_state('/homepage/dashboard/dashboard', 'dashboard');
 });
 
 // redirection ajax when click on discount
 $(document).on('click', '.discount', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/discount/discount.php');
+    handle_redirection_and_push_state('/discount/discount', 'discount');
 });
 
-// redirection ajax for the user detials
-$(document).on('click', '.users', function (e) {
+// redirection ajax when click on products
+$(document).on('click', '.products', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/user_detail/user_detail.php');
+    handle_redirection_and_push_state('/products/products', 'products');
 });
 
 // redirection ajax for the user detials
 $(document).on('click', '.roles', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/roles/roles.php');
+    handle_redirection_and_push_state('/roles/roles', 'roles');
+});
+
+// redirection ajax for the user detials
+$(document).on('click', '.users', function (e) {
+    e.preventDefault();
+    handle_redirection_and_push_state('/user_detail/user_detail', 'user_detail');
 });
 
 // redirection ajax for the category title
 $(document).on('click', '.category_title', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/category_title/category_title.php');
+    handle_redirection_and_push_state('/category_title/category_title', 'category_title');
 });
 
 // redirection ajax for the category heading
 $(document).on('click', '.category_heading', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/category_header/category_header.php');
+    handle_redirection_and_push_state('/category_header/category_header', 'category_header');
 });
 
 // redirection ajax for the category types
 $(document).on('click', '.categories_types', function (e) {
     e.preventDefault();
-    handle_ajax_redirection('/admin/category_types/category_types.php');
+    handle_redirection_and_push_state('/category_types/category_types', 'category_types');
+});
+
+$(document).ready(function () {
+    var query_param = window.location.search;
+    if (query_param.includes('dashboard')) {
+        handle_ajax_redirection('/admin/homepage/dashboard/dashboard.php');
+    } else if (query_param.includes('discount')) {
+        handle_ajax_redirection('/admin/discount/discount.php');
+    } else if (query_param.includes('products')) {
+        handle_ajax_redirection('/admin/products/products.php');
+    } else if (query_param.includes('roles')) {
+        handle_ajax_redirection('/admin/roles/roles.php');
+    } else if (query_param.includes('user_detail')) {
+        handle_ajax_redirection('/admin/user_detail/user_detail.php');
+    } else if (query_param.includes('category_title')) {
+        handle_ajax_redirection('/admin/category_title/category_title.php');
+    } else if (query_param.includes('category_header')) {
+        handle_ajax_redirection('/admin/category_header/category_header.php');
+    } else if (query_param.includes('category_types')) {
+        handle_ajax_redirection('/admin/category_types/category_types.php');
+    }
 });
