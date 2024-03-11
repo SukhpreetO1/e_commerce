5b3738e522c724d255ca7331f8904f92610027f5
SukhpreetO1 <ssingh77022@gmail.com>
2024-03-11 09:34:07 +0530

    fixed the issue of import category types

--- admin/category_types/category_types_import/category_types_import.php
+++ admin/category_types/category_types_import/category_types_import.php
@@ -29,11 +29,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
          return $category_id;
       }
 
-      function get_category_header_id_from_name($database_connection, $category_header_name)
+      function get_category_header_id_from_name($database_connection, $category_header_name, $category_id)
       {
-         $query = "SELECT id FROM categories WHERE name = ?";
+         $query = "SELECT id FROM categories_heading WHERE name = ? AND categories_id = ?";
          $stmt = mysqli_prepare($database_connection, $query);
-         mysqli_stmt_bind_param($stmt, "s", $category_header_name);
+         mysqli_stmt_bind_param($stmt, "si", $category_header_name, $category_id);
          mysqli_stmt_execute($stmt);
          mysqli_stmt_bind_result($stmt, $category_header_id);
          mysqli_stmt_fetch($stmt);
@@ -45,36 +45,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $category_name = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
          $category_id = get_category_id_from_name($database_connection, $category_name);
 
-         $category_header_name = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
-         $category_header_id = get_category_header_id_from_name($database_connection, $category_header_name);
-         
-         $name = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
-         $names[] = "('$category_id', '$name')";
+         $category_header_name = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+         $category_header_id = get_category_header_id_from_name($database_connection, $category_header_name, $category_id);
+
+         $name = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
+         $names[] = "('$category_id', '$category_header_id', '$name')";
       }
 
       if (!empty($names)) {
          $inserted_names = array();
          $failed_names = array();
 
-         $check_sql = "SELECT categories_id, name FROM categories_heading WHERE categories_id = ? AND name = ?";
+         $check_sql = "SELECT category_heading_id, name FROM categories_type WHERE category_heading_id = ? AND name = ?";
          $check_stmt = mysqli_prepare($database_connection, $check_sql);
 
          foreach ($names as $name) {
-            preg_match('/\'([^\']+)\'/', $name, $matches);
-            if (!empty($matches)) {
-               $category_id = $matches[1];
-               $name = str_replace($matches[0], '', $name);
-            } else {
-               list($category_id, $name) = explode(',', trim($name, "()"));
-            }
+            $parts = explode(',', $name);
+            $category_header_id = str_replace("'", '', trim($parts[1]));
+            $name = str_replace("'", '', ltrim(trim($parts[2], ")")));
 
-            $name = trim(trim($name, "()"), ", '");
-            mysqli_stmt_bind_param($check_stmt, "is", $category_id, $name);
+            mysqli_stmt_bind_param($check_stmt, "is", $category_header_id, $name);
             mysqli_stmt_execute($check_stmt);
             mysqli_stmt_store_result($check_stmt);
 
             if (mysqli_stmt_num_rows($check_stmt) == 0) {
-               $inserted_names[] = [$category_id, $name];
+               $inserted_names[] = [$category_header_id, $name];
             } else {
                $failed_names[] = $name;
             }
@@ -82,13 +77,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
          if (!empty($inserted_names)) {
             $values = implode(',', array_map(function ($value) {
-               $category_id = $value[0];
+               $category_header_id = $value[0];
                $name = $value[1];
 
-               return "($category_id,$name)";
+               return "($category_header_id,$name)";
             }, $inserted_names));
 
-            $stmt = mysqli_prepare($database_connection, "INSERT INTO categories_heading (categories_id, name) VALUES (?, ?)");
+            $stmt = mysqli_prepare($database_connection, "INSERT INTO categories_type (category_heading_id, name) VALUES (?, ?)");
             foreach ($inserted_names as $value) {
                mysqli_stmt_bind_param($stmt, 'is', $value[0], $value[1]);
                mysqli_stmt_execute($stmt);
@@ -96,7 +91,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             mysqli_stmt_close($stmt);
 
             if ($stmt) {
-               $brands_directory = dirname(__DIR__, 3) . '/public/assets/uploaded_files/categories_heading/';
+               $brands_directory = dirname(__DIR__, 3) . '/public/assets/uploaded_files/categories_type/';
                if (!file_exists($brands_directory)) {
                   mkdir($brands_directory, 0777, true);
                }
@@ -106,10 +101,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                   chmod($brands_directory, 0777);
                }
 
-               move_uploaded_file($_FILES['file']['tmp_name'], dirname(__DIR__, 3) . '/public/assets/uploaded_files/categories_heading/' . $file_name);
-               $response['success'] = "Category header name imported successfully";
+               move_uploaded_file($_FILES['file']['tmp_name'], dirname(__DIR__, 3) . '/public/assets/uploaded_files/categories_type/' . $file_name);
+               $response['success'] = "Category types name imported successfully";
             } else {
-               $response['error'] = "Failed to import categories heading names";
+               $response['error'] = "Failed to import categories types names";
             }
          } else {
             $response['error'] = "Names that already exists in the database are : " . implode(', ', $failed_names);
